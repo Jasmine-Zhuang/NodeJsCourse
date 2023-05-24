e231c33b478710bc1ec5d197c16ba045a40f9935
Jasmine Zhuang <jasmine.zhuang@mail.utoronto.ca>
2023-05-24 11:54:19 -0400

    add geocode.js under new utils folder

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,4 +1,5 @@
 const request = require('request')
+const geocode = require('./utils/geocode')
 // const url = 'http://api.weatherstack.com/current?access_key=f13a610a76880170363a72b263cc580e&query=37.8267,-122.4233&units=m'
 // request({ url:url, json: true }, (error, response) => {
 //     //in option, have json:true parse response for us
@@ -11,17 +12,20 @@ const request = require('request')
 //     }
 // })
 
-const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiamFzbWluZS14ZXJvIiwiYSI6ImNsaTBvdXB4dDAzcnEzZG85b2w3czF2cnoifQ.3gCgCqNQc-BRw7DNmGlQbQ&limit=1'
-request({ url: geocodeURL, json: true }, (error, response) => {
-    if(error){
-        console.log('Unable to connect to location services!')
-    }else if(response.body.features.length==0){
-        console.log('Unable to find location. Try another search.')
-    }else{
-        const latitude = response.body.features[0].center[1]
-        const longitude = response.body.features[0].center[0]
-        console.log('Longitude is: ' + longitude + ' Latitude is: ' + latitude)
-    }
-    
-})
+// const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiamFzbWluZS14ZXJvIiwiYSI6ImNsaTBvdXB4dDAzcnEzZG85b2w3czF2cnoifQ.3gCgCqNQc-BRw7DNmGlQbQ&limit=1'
+// request({ url: geocodeURL, json: true }, (error, response) => {
+//     if(error){
+//         console.log('Unable to connect to location services!')
+//     }else if(response.body.features.length==0){
+//         console.log('Unable to find location. Try another search.')
+//     }else{
+//         const latitude = response.body.features[0].center[1]
+//         const longitude = response.body.features[0].center[0]
+//         console.log('Longitude is: ' + longitude + ' Latitude is: ' + latitude)
+//     } 
+// })
 
+geocode('Toronto',(error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+})
