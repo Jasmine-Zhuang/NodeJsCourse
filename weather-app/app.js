2d75f8407a533dc8b88eba6af07732fe4554d0b5
Jasmine Zhuang <jasmine.zhuang@mail.utoronto.ca>
2023-05-26 15:58:14 -0400

    destructure object in weather-app

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,44 +1,20 @@
 const request = require('request')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
-// const url = 'http://api.weatherstack.com/current?access_key=f13a610a76880170363a72b263cc580e&query=37.8267,-122.4233&units=m'
-// request({ url:url, json: true }, (error, response) => {
-//     //in option, have json:true parse response for us
-//     if(error){
-//         console.log('Unable to connect to weather services!')
-//     }else if(response.body.error){
-//         console.log('Unable to find location')
-//     }else{
-//         console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out. It feels like ' + response.body.current.feelslike + ' degrees out.')
-//     }
-// })
-
-// const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiamFzbWluZS14ZXJvIiwiYSI6ImNsaTBvdXB4dDAzcnEzZG85b2w3czF2cnoifQ.3gCgCqNQc-BRw7DNmGlQbQ&limit=1'
-// request({ url: geocodeURL, json: true }, (error, response) => {
-//     if(error){
-//         console.log('Unable to connect to location services!')
-//     }else if(response.body.features.length==0){
-//         console.log('Unable to find location. Try another search.')
-//     }else{
-//         const latitude = response.body.features[0].center[1]
-//         const longitude = response.body.features[0].center[0]
-//         console.log('Longitude is: ' + longitude + ' Latitude is: ' + latitude)
-//     } 
-// })
 const address = process.argv[2]
 if(!address){
   console.log('Please provide an address.')
 }else{
-  geocode(address,(error, data) => {
+  geocode(address,(error, {latitude, longitude, location} = {}) => {
   if(error){
     return console.log(error)
   }
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
+    forecast(latitude, longitude, (error, forecastData) => {
       if(error){
         return console.log(error)
       }
 
-      console.log(data.location)
+      console.log(location)
       console.log(forecastData)
     })
 })
