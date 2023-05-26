6c86fdbcbe1abc3a2c442e7897d6d1babc08346a
Jasmine Zhuang <jasmine.zhuang@mail.utoronto.ca>
2023-05-26 16:31:07 -0400

    add routes

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const app = express()
-//app.com
-//app.com/help
-//app.com/about
 app.get('', (req, res) => {
     res.send('Hello express!')
 })
 app.get('/help', (req, res)=>{
     res.send('Help page')
+})
+app.get('/about', (req, res)=>{
+    res.send('About')
+})
+app.get('/weather', (req, res)=>{
+    res.send('Weather')
 })
 app.listen(3000, ()=> {
     console.log('Server is up on port 3000.')
