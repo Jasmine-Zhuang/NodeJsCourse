1848cd2bbd975b38bfa7d1d1155f02f11205210a
Jasmine Zhuang <jasmine.zhuang@mail.utoronto.ca>
2023-05-29 10:56:14 -0400

    add about and help html

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,16 +3,9 @@ const express = require('express')
 
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
+
 app.use(express.static(publicDirectoryPath))
 
-app.get('/help', (req, res)=>{
-    res.send([{
-        name: 'Jas'
-    },{name:'Henry'}])
-})
-app.get('/about', (req, res)=>{
-    res.send('<h1>About</h1>')
-})
 app.get('/weather', (req, res)=>{
     res.send([{forecast:'It is sunny', location:'Toronto'}])
 })
