bd5dbbf285c58782e7a90caa5cfb6eed28cfba08
Jasmine Zhuang <jasmine.zhuang@mail.utoronto.ca>
2023-05-29 11:31:44 -0400

    add hbs template for index(localhost 3000)

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,7 +4,15 @@ const express = require('express')
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Jasmine Z'
+    })
+})
 
 app.get('/weather', (req, res)=>{
     res.send([{forecast:'It is sunny', location:'Toronto'}])
