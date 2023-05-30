e59e4e8fd938dae176fd2956d84d25846d642212
Jasmine Zhuang <jasmine.zhuang@mail.utoronto.ca>
2023-05-30 11:18:04 -0400

    add weather search form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,13 +1,21 @@
 console.log('Client side javascript file is loaded')
 
-fetch("http://localhost:3000/weather?address=toronto").then((res) => {
-  res.json().then((data) => {
-    if(data.error){
-        console.log(data.error)
-    }else{
-        console.log(data.location)
-        console.log(data.forecast)
-    }
-    
-  })
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const location = search.value
+
+    fetch("http://localhost:3000/weather?address="+location).then((res) => {
+      res.json().then((data) => {
+        if (data.error) {
+          console.log(data.error);
+        } else {
+          console.log(data.location);
+          console.log(data.forecast);
+        }
+      });
+    });
+
 })
