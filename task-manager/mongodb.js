3b8c945bf7926f4584ea26de2b48c4806d28cffe
Jasmine Zhuang <jasmine.zhuang@mail.utoronto.ca>
2023-06-01 10:51:00 -0400

    add tasks find and findOne

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -14,19 +14,29 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
         return console.log('Unable to connect to database.')
     } 
     const db = client.db(databaseName)
+    db.collection('tasks').findOne({_id: new ObjectId('6477ae18eddeeb39fbe11325')}, (error, task) => {
+        console.log(task)
+    })
+    db.collection('tasks').find({completed: false}).toArray((error, tasks) => {
+        console.log(tasks)
+    })
+
+
     // db.collection('users').findOne({_id: new ObjectId("6477afbed0eb8051853f83b8")}, (error, user) => {//get the first one if there's duplicate
     //     if(error){
     //         return console.log('Unable to fetch')
     //     }
     //     console.log(user)
     // })
+
     //return a cursor 
-    db.collection('users').find({age: 21}).toArray((error, users) => {
-        console.log(users)
-    })
-    db.collection('users').find({age: 21}).count((error, count) => {
-        console.log(count)
-    })
+    // db.collection('users').find({age: 21}).toArray((error, users) => {
+    //     console.log(users)
+    // })
+    // db.collection('users').find({age: 21}).count((error, count) => {
+    //     console.log(count)
+    // })
+
     // db.collection('users').insertOne({
     //     _id : id ,name: "Mia", age: 21
     // }, (error, result) => {
