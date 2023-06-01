438c4b4383090bff86ac779e46f8ffa79a9b227e
Jasmine Zhuang <jasmine.zhuang@mail.utoronto.ca>
2023-06-01 14:06:20 -0400

    install validator module and add validation

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,35 +1,55 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true
 })
 const User = mongoose.model('User', {
     name: {
-        type:String
+        type:String,
+        required: true,
+        trim: true //remove whitespace left and right
+    },
+    email:{
+        type: String,
+        required:true,
+        trim: true,
+        lowercase:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Email is invalid')
+            }
+        }
     },
     age: {
-        type:Number
+        type:Number,
+        default:0,
+        validate(value){
+            if(value < 0){
+                throw new Error('Age must be a positive')
+            }
+        }
     }
 })
-// const me = new User({name: 'Jasmine', age: 21})
-// me.save().then(() => {
-//     console.log(me)
-// }).catch((error)=>{
-//     console.log('Error!',error)
+const me = new User({name: 'Henry ',email:'ben@gmail.com   ', age: 19})
+me.save().then(() => {
+    console.log(me)
+}).catch((error)=>{
+    console.log('Error!',error)
+})
+// const Task = mongoose.model('Task', {
+//     description:{
+//         type: String
+//     }, completed: {
+//         type: Boolean
+//     } 
 // })
-const Task = mongoose.model('Task', {
-    description:{
-        type: String
-    }, completed: {
-        type: Boolean
-    } 
-})
-const cleanHouse = new Task({
-    description:'clean the house', 
-    completed: false
-})
-cleanHouse.save().then(() => {
-    console.log(cleanHouse)
-}).catch((error) => {
-        console.log('error',error)
-    })
+// const cleanHouse = new Task({
+//     description:'clean the house', 
+//     completed: false
+// })
+// cleanHouse.save().then(() => {
+//     console.log(cleanHouse)
+// }).catch((error) => {
+//         console.log('error',error)
+//     })
