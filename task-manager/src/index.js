472b067af8ca223e73038f570f2abd4f055b9d34
Jasmine Zhuang <jasmine.zhuang@mail.utoronto.ca>
2023-06-02 14:20:36 -0400

    add read tasks and task by id enpoints

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -19,7 +19,7 @@ app.post('/users', (req, res) => {
 })
 
 app.get('/users', (req, res) => {
-    User.find({name:"Jas"}).then((users) => {
+    User.find({}).then((users) => {
         res.send(users)
     }).catch((e) => {
         res.status(500).send()
@@ -35,8 +35,7 @@ app.get('/users/:id', (req, res) => {
         res.send(user)
     }).catch((e) => {
         res.status(500).send()
-    })
-    
+    })   
 })
 
 app.post('/tasks', (req, res) => {
@@ -47,6 +46,26 @@ app.post('/tasks', (req, res) => {
         res.status(400).send(error)
         
     })
+})
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+    Task.findById(_id).then((task) => {
+        if(!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+    }).catch((e) => {
+        res.status(500).send()
+    })   
 })
 
 app.listen(port, () => {
