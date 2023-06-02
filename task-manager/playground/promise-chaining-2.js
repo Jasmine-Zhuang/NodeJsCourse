5cc711e7ac14e3f93948c2124d2b2a7ffaedbc21
Jasmine Zhuang <jasmine.zhuang@mail.utoronto.ca>
2023-06-02 15:09:20 -0400

    add async and await challenges

--- task-manager/playground/promise-chaining-2.js
+++ task-manager/playground/promise-chaining-2.js
@@ -1,11 +1,20 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-
-Task.findByIdAndRemove('6478dfc8453d0519417db5d3').then((task)=>{
-    console.log(task)
-    return Task.countDocuments({completed:false})
-}).then((result) => {
+// Task.findByIdAndDelete('6478dfc8453d0519417db5d3').then((task)=>{
+//     console.log(task)
+//     return Task.countDocuments({completed:false})
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e)=>{
+//     console.log(e)
+// })
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({completed:false})
+    return count
+}
+deleteTaskAndCount('647a2e4d44dee3dbdb9162bf').then((result) => {
     console.log(result)
 }).catch((e)=>{
     console.log(e)
