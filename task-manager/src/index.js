f930714cf7f7618aa2f00f868ee909a205ccd076
Jasmine Zhuang <jasmine.zhuang@mail.utoronto.ca>
2023-06-05 14:26:11 -0400

    add mongoose middleware for password with bcrypt

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -16,12 +16,3 @@ app.listen(port, () => {
 })
 
 const bcrypt = require('bcryptjs')
-const myFunc = async() => {
-    const password = 'Red12345!'
-    const hashedPassword = await bcrypt.hash(password, 8)
-    console.log(password)
-    console.log(hashedPassword)
-    const isMatch = await bcrypt.compare(password, hashedPassword)
-    console.log(isMatch)
-}
-myFunc()
