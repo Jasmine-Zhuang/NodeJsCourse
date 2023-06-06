4ae88b496f732eecdcf7ce932b6065df667cb03f
Jasmine Zhuang <jasmine.zhuang@mail.utoronto.ca>
2023-06-06 16:18:43 -0400

    login user

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -10,9 +10,10 @@ const userSchema = new mongoose.Schema({
     },
     email:{
         type: String,
-        required:true,
+        unique: true,
+        required: true,
         trim: true,
-        lowercase:true,
+        lowercase: true,
         validate(value){
             if(!validator.isEmail(value)){
                 throw new Error('Email is invalid')
@@ -40,6 +41,20 @@ const userSchema = new mongoose.Schema({
         }
     }
 })
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+    if(!user){
+        throw new Error('Unable to login')
+    }
+    const isMatch = await bcrypt.compare(password, user.password)
+    if(!isMatch){
+        throw new Error('Unable to login')
+    }
+    return user
+}
+
+//Hash the plain text password before saving
 userSchema.pre('save', async function (next) {
     const user = this
     if (user.isModified('password')){
