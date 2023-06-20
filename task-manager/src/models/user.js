f7bf9ad63df6faced982771befc986461aee6ef1
Jasmine Zhuang <jasmine.zhuang@mail.utoronto.ca>
2023-06-20 14:50:00 -0400

    override user.toJSON to hide private data

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -57,13 +57,14 @@ userSchema.methods.generateAuthToken = async function(){
     return token
 }
 
-userSchema.methods.getPublicProfile = function() {
+userSchema.methods.toJSON = function() {
     const user = this
     const userObject = user.toObject()
     delete userObject.password
     delete userObject.tokens
     return userObject
 }
+
 //model method (User)
 userSchema.statics.findByCredentials = async (email, password) => {
     const user = await User.findOne({ email })
