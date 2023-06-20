dd00d0e3bb065d69ecb9f17314aa9740e7399c96
Jasmine Zhuang <jasmine.zhuang@mail.utoronto.ca>
2023-06-20 14:38:20 -0400

    Add logoutAll route

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -37,6 +37,17 @@ router.post('/users/logout', auth, async (req, res) => {
     }
 })
 
+// logout all user tokens
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try{
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    }catch (e) {
+        res.status(500).send()
+    }
+})
+
 router.get('/users/me', auth, async (req, res) => {
     res.send(req.user)
 })
