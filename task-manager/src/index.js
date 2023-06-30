599a862c4a8d08e685c2b5a55a550ddb9d925bd5
Jasmine Zhuang <jasmine.zhuang@mail.utoronto.ca>
2023-06-30 10:27:38 -0400

    update index.js

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,31 +6,10 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-const multer = require('multer')
-const upload = multer({ 
-    dest: 'images',
-    limits: {
-        fileSize: 1000000 // number in byte, 1MB
-    },
-    fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.(doc|docx)$/)){ //!file.originalname.endsWith('.pdf') => please upload a pdf
-            return cb(new Error("Please upload a Word document"))
-        }
-        cb(undefined, true)
-
-        // cb(new Error('File must be a PDF'))
-        // cb(undefined, true)
-        // cb(undefined, false)
-    }
-})
-app.post('/upload', upload.single('upload'), (req, res) => {
-    res.send()
-})
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
 app.listen(port, () => {
-    console.log('server is up on port '+port)
+    console.log('server is up on port ' + port)
 })
