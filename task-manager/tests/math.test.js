0e2fe4682674f5199cf949db823838ca5d7ae8fb
Jasmine Zhuang <jasmine.zhuang@mail.utoronto.ca>
2023-07-11 16:01:08 -0400

    add more math tests

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,4 +1,4 @@
-const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit } = require("../src/math");
+const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit, add } = require("../src/math");
 
 test('Should calculate total with tip', () => {
     expect(calculateTip(10, .3)).toBe(13)
@@ -14,4 +14,16 @@ test("Should convert 32 fahrenheit to 0 celsius", () => {
 
 test("Should convert 0 celsius to 32 fahrenheit ", () => {
   expect(celsiusToFahrenheit(0)).toBe(32)
-});
+})
+
+test('Should add 2 numbers', (done) => {
+    add(1, 3).then((sum) => {
+        expect(sum).toBe(4)
+        done()
+    })
+})
+
+test("Should add 2 numbers async/await", async () => {
+    const sum = await add(2, 3)
+    expect(sum).toBe(5)
+})
