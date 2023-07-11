45bd1c6bbe24d81525e8cb1ae3a0101fedbb8db3
Jasmine Zhuang <jasmine.zhuang@mail.utoronto.ca>
2023-07-11 14:13:41 -0400

    test c to f and f to c funcitons challenge

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,9 +1,17 @@
-const { calculateTip } = require('../src/math')
+const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit } = require("../src/math");
 
 test('Should calculate total with tip', () => {
     expect(calculateTip(10, .3)).toBe(13)
 }) 
 
 test("Should calculate total with default tip", () => {
-  expect(calculateTip(10)).toBe(11.5);
-}); 
+  expect(calculateTip(10)).toBe(11.5)
+})
+
+test("Should convert 32 fahrenheit to 0 celsius", () => {
+  expect(fahrenheitToCelsius(32)).toBe(0)
+})
+
+test("Should convert 0 celsius to 32 fahrenheit ", () => {
+  expect(celsiusToFahrenheit(0)).toBe(32)
+});
