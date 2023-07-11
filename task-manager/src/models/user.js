b72b4ef4fc76847c61c32ec5af591feec2d7f0b3
Jasmine Zhuang <jasmine.zhuang@mail.utoronto.ca>
2023-07-11 16:42:32 -0400

    use dev.env

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -58,7 +58,7 @@ const userSchema = new mongoose.Schema({
 // instance method(user)
 userSchema.methods.generateAuthToken = async function(){
     const user = this
-    const token = jwt.sign({ _id: user._id.toString() }, 'thisismyfirstnodejscourse')
+    const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET)
     user.tokens = user.tokens.concat({ token })
     await user.save()
     return token
