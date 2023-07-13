b9ded20db2d082299491456057bb71dce2ba2df9
Jasmine Zhuang <jasmine.zhuang@mail.utoronto.ca>
2023-07-13 16:09:17 -0400

    delete userDB before each test run

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -1,2 +1,14 @@
 const request = require('supertest')
-const 
+const app = require('../src/app')
+const User = require('../src/models/user')
+beforeEach(async () => {
+    await User.deleteMany()
+})
+
+test('Should signup a new user', async () => {
+    await request(app).post("/users").send({
+        name: "Blue6",
+        email: "blue6@test.com",
+        password: "Computer001",
+      }).expect(201);
+})
