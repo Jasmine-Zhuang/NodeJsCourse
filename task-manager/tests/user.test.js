14a540f59b44f3ac8e7676b903a3e8377095b3c0
Jasmine Zhuang <jasmine.zhuang@mail.utoronto.ca>
2023-07-13 16:13:50 -0400

    update user test

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -1,14 +1,38 @@
 const request = require('supertest')
 const app = require('../src/app')
 const User = require('../src/models/user')
+
+const user1 = {
+    name: 'Mike',
+    email: 'mike@test.com',
+    password: '555what'
+}
+
 beforeEach(async () => {
     await User.deleteMany()
+    await new User(user1).save()
 })
 
 test('Should signup a new user', async () => {
     await request(app).post("/users").send({
-        name: "Blue6",
-        email: "blue6@test.com",
+        name: "Blue",
+        email: "blue@test.com",
         password: "Computer001",
-      }).expect(201);
+      }).expect(201)
+})
+
+test("Should login existing user", async () => {
+  await request(app).post("/users/login").send({
+      email: user1.email,
+      password: user1.password
+    }).expect(200)
+})
+
+test("Should not login non-existing user", async () => {
+  await request(app)
+    .post("/users/login")
+    .send({
+      email: 'nonexistuser@email.com',
+      password: user1.password,
+    }).expect(400)
 })
