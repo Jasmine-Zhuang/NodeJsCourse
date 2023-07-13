42258e467ee6386ea56000d9fd6073a0b92fe992
Jasmine Zhuang <jasmine.zhuang@mail.utoronto.ca>
2023-07-13 16:29:13 -0400

    add user test with auth

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -1,37 +1,81 @@
-const request = require('supertest')
-const app = require('../src/app')
-const User = require('../src/models/user')
+const request = require("supertest");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const app = require("../src/app");
+const User = require("../src/models/user");
 
+const user1Id = new mongoose.Types.ObjectId();
 const user1 = {
-    name: 'Mike',
-    email: 'mike@test.com',
-    password: '555what'
-}
+  _id: user1Id,
+  name: "Mike",
+  email: "mike@test.com",
+  password: "555what",
+  tokens: [
+    {
+      token: jwt.sign({ _id: user1Id }, process.env.JWT_SECRET),
+    },
+  ],
+};
 
 beforeEach(async () => {
-    await User.deleteMany()
-    await new User(user1).save()
-})
+  await User.deleteMany();
+  await new User(user1).save();
+});
 
-test('Should signup a new user', async () => {
-    await request(app).post("/users").send({
-        name: "Blue",
-        email: "blue@test.com",
-        password: "Computer001",
-      }).expect(201)
-})
+test("Should signup a new user", async () => {
+  await request(app)
+    .post("/users")
+    .send({
+      name: "Blue",
+      email: "blue@test.com",
+      password: "Computer001",
+    })
+    .expect(201);
+});
 
 test("Should login existing user", async () => {
-  await request(app).post("/users/login").send({
+  await request(app)
+    .post("/users/login")
+    .send({
       email: user1.email,
-      password: user1.password
-    }).expect(200)
-})
+      password: user1.password,
+    })
+    .expect(200);
+});
 
 test("Should not login non-existing user", async () => {
   await request(app)
-    .post("/users/login").send({
-      email: 'nonexistuser@email.com',
+    .post("/users/login")
+    .send({
+      email: "nonexistuser@email.com",
       password: user1.password,
-    }).expect(400)
-})
+    })
+    .expect(400);
+});
+
+test("Should get profile for user", async () => {
+  await request(app)
+    .get("/users/me")
+    .set("Authorization", `Bearer ${user1.tokens[0].token}`)
+    .send()
+    .expect(200);
+});
+
+test("Should not get profile for unauthenticated user", async () => {
+  await request(app).get("/users/me").send().expect(401);
+});
+
+test("Should delete account for user", async () => {
+  await request(app)
+    .delete("/users/me")
+    .set("Authorization", `Bearer ${user1.tokens[0].token}`)
+    .send()
+    .expect(200);
+});
+
+test("Should not delete account for unauthenticated user", async () => {
+  await request(app)
+    .delete("/users/me")
+    .send()
+    .expect(401);
+});
