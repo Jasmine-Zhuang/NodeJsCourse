1e313016b89dae0e07361603432fa05c835b5990
Jasmine Zhuang <jasmine.zhuang@mail.utoronto.ca>
2023-07-31 11:34:06 -0400

    setup websocket with client, server connect

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
+const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server, {
+  /* options */
+});
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
+
+io.on("connection", (socket) => {
+  console.log('New WebSocket connection')
+});
 
 server.listen(port, () => {
   console.log("server is up on port " + port);
