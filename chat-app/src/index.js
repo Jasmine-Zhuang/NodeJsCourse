477f7835dc71230cdcd74d03aced9266984b2c5e
Jasmine Zhuang <jasmine.zhuang@mail.utoronto.ca>
2023-07-31 11:43:52 -0400

    add +1 button and emit the update to all clients

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const path = require('path');
-const http = require('http');
+const express = require("express");
+const path = require("path");
+const http = require("http");
 const { Server } = require("socket.io");
 
 const app = express();
@@ -10,12 +10,23 @@ const io = new Server(server, {
 });
 
 const port = process.env.PORT || 3000;
-const publicDirectoryPath = path.join(__dirname, '../public');
+const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
+let count = 0;
+
 io.on("connection", (socket) => {
-  console.log('New WebSocket connection')
+  console.log("New WebSocket connection");
+
+  socket.emit("countUpdated", count);
+  socket.on('increment', () => {
+    count++;
+    // socket.emit('countUpdated', count);
+
+    // emit to all connections
+    io.emit('countUpdated', count);
+  })
 });
 
 server.listen(port, () => {
