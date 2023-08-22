c738b4f3ac6984e07551dda8a2e1b4482caf0b77
Jasmine Zhuang <jasmine.zhuang@mail.utoronto.ca>
2023-08-22 14:52:01 -0400

    Add code to acknowledge events

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -5,19 +5,30 @@ socket.on("message", (message) => {
 });
 
 document.querySelector("#message-form").addEventListener("submit", (e) => {
-  e.preventDefault(); //prevent default browser refresh
+  e.preventDefault(); // prevent default browser refresh
   const message = e.target.elements.message.value;
-  socket.emit("sendMessage", message);
+  socket.emit("sendMessage", message, (error) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Message delivered.");
+  });
 });
 
 document.querySelector("#send-location").addEventListener("click", () => {
   if (!navigator.geolocation) {
-    return alert("geolocation is not supported by your browser");
+    // use mdn geolocation
+    return alert("geolocation is not supported by your browser.");
   }
   navigator.geolocation.getCurrentPosition((position) => {
     socket.emit("sendLocation", {
       long: position.coords.longitude,
       lat: position.coords.latitude,
+    }, (error) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Location shared.')
     });
   });
 });
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const http = require("http");
 const { Server } = require("socket.io");
-
+const Filter = require("bad-words");
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
@@ -14,24 +14,27 @@ app.use(express.static(publicDirectoryPath));
 
 io.on("connection", (socket) => {
   // socket === client
-  console.log("New WebSocket connection");
+  console.log("New WebSocket connection.");
 
   socket.emit("message", "Welcome!"); // send to single client
-  socket.broadcast.emit("message", "A new user has joined"); // send to all clients except this socket
+  socket.broadcast.emit("message", "A new user has joined."); // send to all clients except this socket
 
-  socket.on("sendMessage", (message) => {
+  socket.on("sendMessage", (message, callback) => {
+    const filter = new Filter();
+    if (filter.isProfane(message)) {
+      return callback("Profanity is not allowed.");
+    }
     io.emit("message", message); // send to all clients
+    callback();
   });
 
-  socket.on("sendLocation", (location) => {
-    io.emit(
-      "message",
-      `https://google.com/maps?q=${location.lat},${location.long}`
-    );
+  socket.on("sendLocation", (location, callback) => {
+    io.emit("message", `https://google.com/maps?q=${location.lat},${location.long}`);
+    callback();
   });
 
   socket.on("disconnect", () => {
-    io.emit("message", "A user has left"); // send to all clients
+    io.emit("message", "A user has left."); // send to all clients
   });
 });
 
