52fe0fbd930e6f71f42809d3aa07a0c7d4987d52
Jasmine Zhuang <jasmine.zhuang@mail.utoronto.ca>
2023-09-25 10:19:19 -0400

    form and button states

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,13 +1,27 @@
 const socket = io();
 
+// Elements
+const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
+const $sendLocationButton = document.querySelector("#send-location");
+
 socket.on("message", (message) => {
   console.log(message);
 });
 
-document.querySelector("#message-form").addEventListener("submit", (e) => {
+$messageForm.addEventListener("submit", (e) => {
   e.preventDefault(); // prevent default browser refresh
+  // disable form button once it's been submitted already
+  $messageFormButton.setAttribute("disabled", "disabled");
+
   const message = e.target.elements.message.value;
   socket.emit("sendMessage", message, (error) => {
+    // enable form button
+    $messageFormButton.removeAttribute("disabled");
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
+
     if (error) {
       return console.log(error);
     }
@@ -15,20 +29,25 @@ document.querySelector("#message-form").addEventListener("submit", (e) => {
   });
 });
 
-document.querySelector("#send-location").addEventListener("click", () => {
+$sendLocationButton.addEventListener("click", () => {
   if (!navigator.geolocation) {
     // use mdn geolocation
     return alert("geolocation is not supported by your browser.");
   }
+  // disable button
+  $sendLocationButton.setAttribute("disabled", "disabled");
+
   navigator.geolocation.getCurrentPosition((position) => {
-    socket.emit("sendLocation", {
-      long: position.coords.longitude,
-      lat: position.coords.latitude,
-    }, (error) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Location shared.')
-    });
+    socket.emit(
+      "sendLocation",
+      {
+        long: position.coords.longitude,
+        lat: position.coords.latitude,
+      },
+      (error) => {
+        $sendLocationButton.removeAttribute("disabled");
+        console.log("Location shared.");
+      }
+    );
   });
 });
